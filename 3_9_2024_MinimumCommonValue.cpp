1f1810abb04bb586d758fcb0001b7971336bbc6a
Logan Bryant <64882175+rhit-bryantlj@users.noreply.github.com>
2024-03-11 12:16:44 -0400

    Create 3_9_2024_MinimumCommonValue.cpp

---
+++ 3_9_2024_MinimumCommonValue.cpp
@@ -0,0 +1,26 @@
+/*
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+
+Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+Beats 49.23% of users with C++: Runtime
+Beats 81.50% of users with C++: Memory
+*/
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0;
+        int j = 0;
+
+        while(i < nums1.size() && j < nums2.size()){
+            if(nums1[i] == nums2[j])
+                return nums1[i];
+            if(nums1[i] < nums2[j])
+                i++;
+            else 
+                j++;
+        }
+        return -1;
+    }
+};
